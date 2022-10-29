ee2a59afc84a52f8d2be5bc247f7e472a48c5124
Kyrylo Karnyshov <kyrylo.karnyshov@gmail.com>
2022-10-29 14:26:28 +0300

    Homework for lesson #11

---
+++ homework_11/task_1.py
@@ -0,0 +1,27 @@
+"""
+Создать декоратор которые принтит в консоль имя функции которая была вызвана.
+Функция при этом должна работать как и задумывалось.
+К примеру создайте пару функции для арифметических операций суммирования и умножения.
+При вызове этих функций должен возвращаться результат операции и предварительно выводиться в консоль имя функции которая
+была вызвана.
+"""
+
+
+def func_name(function):
+    def name(a, b):
+        return f"{function.__name__}\n{function(a,b)}"
+    return name
+
+
+@func_name
+def add(first: int, second: int) -> int:
+    return first + second
+
+
+@func_name
+def subtraction(first: int, second: int) -> int:
+    return first - second
+
+
+print(add(2, 2))
+print(subtraction(3, 2))
---
+++ homework_11/task_2.py
@@ -0,0 +1,26 @@
+"""
+Создайте функцию которая способна вернуть квадраты четных элементов для диапазона от 0 до 1000000000 включительно.
+Решение должно отрабатывать и не фризить ваш комп.
+"""
+
+
+def square_item(length: int):
+    squared = []
+    counter = 0
+
+    while counter < length:
+        if counter % 2 == 0:
+            squared.append(counter ** 2)
+        yield squared
+        counter += 1
+
+    return squared
+
+
+generator = square_item(1000000000)
+
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
