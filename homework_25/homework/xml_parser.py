85c8efd5ea1d2449c647d896615eca8201796bd9
Kyrylo Karnyshov <kyrylo.karnyshov@gmail.com>
2023-06-20 15:36:34 +0300

    Homework for lesson #25 Fixed parser

--- homework_25/homework/xml_parser.py
+++ homework_25/homework/xml_parser.py
@@ -17,34 +17,18 @@ root = tree.getroot()
 
 movies_list = []
 
-# TODO: fix list appending and duplicate
 for genre in root.findall("genre"):
-    # movies_list.append(movie_fields["category"])
-    category = {"category": genre.attrib["category"]}
-    # print(category)
-    # movies_list.append({"category": genre.attrib["category"]})
+    category = genre.attrib["category"]
     for decades in genre.findall("decade"):
-        # movies_list.append({"decade": decades.attrib["years"]})
-        decade = {"decade": decades.attrib["years"]}
-        # print(decade)
-        # category.update(decade)
-        # print(category)
+        decade = decades.attrib["years"]
         for movie in decades.findall("movie"):
-            # category.update(decade)
-            tmp = {"title": movie.attrib["title"],
+            tmp = {"category": category,
+                   "decade": decade,
+                   "title": movie.attrib["title"],
                    "format": movie.find("format").text,
                    "year": movie.find("year").text,
                    "rating": movie.find("rating").text,
                    "description": movie.find("description").text}
-            # category.update(decade)
-            # print(category)
-            # category.update(tmp)
-            # print(category)
-            decade.update(tmp)
-            category.update(decade)
-            print(category)
-            # print(decade)
-            # movies_list.append(tmp)
-            # print(movies_list)
+            movies_list.append(tmp)
 
-# print(movies_list)
+print(movies_list)
