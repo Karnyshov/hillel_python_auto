a98cceb0f0720c8d65c4444c3d96d7f3c70e565c
Kyrylo Karnyshov <kyrylo.karnyshov@gmail.com>
2023-06-20 15:21:14 +0300

    Homework for lesson #25 Ongoing Fixes

--- homework_25/homework/xml_parser.py
+++ homework_25/homework/xml_parser.py
@@ -16,19 +16,35 @@ tree = ElementTree.parse("movies.xml")
 root = tree.getroot()
 
 movies_list = []
-movie_fields = {}
 
 # TODO: fix list appending and duplicate
 for genre in root.findall("genre"):
-    movie_fields["category"] = genre.attrib["category"]
-    for decade in genre.findall("decade"):
-        movie_fields["decade"] = decade.attrib["years"]
-        for movie in decade.findall("movie"):
-            movie_fields["title"] = movie.attrib["title"]
-            movie_fields["format"] = movie.find("format").text
-            movie_fields["year"] = movie.find("year").text
-            movie_fields["rating"] = movie.find("rating").text
-            movie_fields["description"] = movie.find("description").text
-            print(movie_fields)
-            movies_list.append(movie_fields)
-            print(movies_list)
+    # movies_list.append(movie_fields["category"])
+    category = {"category": genre.attrib["category"]}
+    # print(category)
+    # movies_list.append({"category": genre.attrib["category"]})
+    for decades in genre.findall("decade"):
+        # movies_list.append({"decade": decades.attrib["years"]})
+        decade = {"decade": decades.attrib["years"]}
+        # print(decade)
+        # category.update(decade)
+        # print(category)
+        for movie in decades.findall("movie"):
+            # category.update(decade)
+            tmp = {"title": movie.attrib["title"],
+                   "format": movie.find("format").text,
+                   "year": movie.find("year").text,
+                   "rating": movie.find("rating").text,
+                   "description": movie.find("description").text}
+            # category.update(decade)
+            # print(category)
+            # category.update(tmp)
+            # print(category)
+            decade.update(tmp)
+            category.update(decade)
+            print(category)
+            # print(decade)
+            # movies_list.append(tmp)
+            # print(movies_list)
+
+# print(movies_list)
